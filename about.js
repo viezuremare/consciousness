82c7d2e38b0edc42dad6460002f0155a1457e714
mirel <mirelbecheanu@gmx.com>
2019-05-31 06:59:06 +0300

    footer buttons hover about

---
+++ about.js
@@ -0,0 +1,12 @@
+const t7 = document.getElementById('text7')
+const t8 = document.getElementById('text8')
+
+t7.addEventListener('mouseover', () => {
+    t7.style.cursor = 'pointer'
+    t7.style.backgroundColor = 'rgba(3, 95, 95, 0.2)'
+})
+
+t8.addEventListener('mouseover', () => {
+    t8.style.cursor = 'pointer'
+    t8.style.backgroundColor = 'rgba(3, 95, 95, 0.2)'
+})
