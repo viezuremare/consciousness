129f4f4c21b4da3c3a3ac61381a3f9ecd8cf6878
mirel <mirelbecheanu@gmx.com>
2019-05-31 07:14:08 +0300

    mouseenter mouseleave footer buttons about

--- about.js
+++ about.js
@@ -1,12 +1,22 @@
 const t7 = document.getElementById('text7')
 const t8 = document.getElementById('text8')
 
-t7.addEventListener('mouseover', () => {
+t7.addEventListener('mouseenter', () => {
     t7.style.cursor = 'pointer'
     t7.style.backgroundColor = 'rgba(3, 95, 95, 0.2)'
+})
+
+t7.addEventListener('mouseleave', () => {
+    t7.style.cursor = 'default'
+    t7.style.backgroundColor = 'darkcyan'
 })
 
 t8.addEventListener('mouseover', () => {
     t8.style.cursor = 'pointer'
     t8.style.backgroundColor = 'rgba(3, 95, 95, 0.2)'
+})
+
+t8.addEventListener('mouseleave', () => {
+    t8.style.cursor = 'default'
+    t8.style.backgroundColor = 'darkcyan'
 })
